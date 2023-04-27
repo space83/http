c9558f9873e7d8ce4dcd9e404868ce84c7bcd7a2
mazlan.ibrahim <mazlan.ibrahim@uberfusion.com>
2023-04-27 16:26:34 +0800

    9.8

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,23 +6,48 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
+
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                console.log(response);
-            });
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        autor: 'Max'
+                    }
+                });
+                this.setState({posts: response.data});
+                //console.log(response);
+            } );
+        }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
     }
 
     render () {
+        const posts = this.state.posts.map(
+            post => {
+                return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author} 
+                clicked={() => this.postSelectedHandler(post.id)} />;
+            }
+        );
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
